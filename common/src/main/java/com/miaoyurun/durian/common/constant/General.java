ea60d5299e9bc3f9b3a4c8a4d4a5975cafd04813
miaoyurun <miaoyurun@bytedance.com>
2024-09-15 12:51:48 +0800

    添加模块 common，并添加通用常量类

---
+++ common/src/main/java/com/miaoyurun/durian/common/constant/General.java
@@ -0,0 +1,5 @@
+package com.miaoyurun.durian.common.constant;
+
+public class General {
+    public static final String Empty = "";
+}
---
+++ common/src/test/java/com/miaoyurun/durian/common/constant/GeneralTest.java
@@ -0,0 +1,11 @@
+package com.miaoyurun.durian.common.constant;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GeneralTest {
+    @Test
+    public void testGeneral() {
+        Assertions.assertEquals("", General.Empty);
+    }
+}
